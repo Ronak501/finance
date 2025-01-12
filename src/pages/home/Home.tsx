c7b52022eebeb98c0f23f4f09e43c55a044bd862
Ronak501 <ronaktalaviya51@gmail.com>
2025-01-13 00:02:51 +0530

    fixs bugs

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -266,7 +266,7 @@ function App() {
 
       {/* Graph Section */}
       <div className="bg-white py-20">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-6">
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
           <NiftyIndicesBarGraph />
         </div>
       </div>
